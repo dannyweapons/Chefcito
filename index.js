8ceca9b1a6fe7823706adfce6365f1733a6c78f3
dannyweapons <danytazps@gmail.com>
2016-11-12 18:34:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post('/webhook/', function (req, res) {
 
 // recommended to inject access tokens as environmental variables, e.g.
 // const token = process.env.PAGE_ACCESS_TOKEN
-const token = "<PAGE_ACCESS_TOKEN>"
+const token = "<1434584383235775>"
 
 function sendTextMessage(sender, text) {
 	let messageData = { text:text }
